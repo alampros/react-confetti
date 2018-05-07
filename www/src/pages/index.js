fc8d5d4cf6c66bd997b2c60ecd7542874e15afb3
Aaron Lampros <alampros@gmail.com>
2018-05-07 15:10:43 -0400

    www: fix z-index stack

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -6,13 +6,13 @@ export default class Index extends React.PureComponent {
   render() {
     return (
       <div className="app-container">
+        <SizedConfetti />
         <div className="pad-container">
           <h1>react-confetti</h1>
           <p>
             <a href="http://github.com/alampros/react-confetti">http://github.com/alampros/react-confetti</a>
           </p>
         </div>
-        <SizedConfetti />
       </div>
     )
   }
