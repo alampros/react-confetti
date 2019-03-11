e7344331e38f79fd37c706d69f5ba9f2ab53c5bc
Sean Coker <1030830+okcoker@users.noreply.github.com>
2019-03-10 20:31:20 -0400

    Fix circle typo

--- src/Particle.ts
+++ src/Particle.ts
@@ -2,7 +2,7 @@ import { randomRange, randomInt, degreesToRads } from './utils'
 import { IConfettiOptions } from './Confetti'
 
 export enum ParticleShape {
-  Cirlce = 0,
+  Circle = 0,
   Square,
   Strip,
 }
@@ -75,7 +75,7 @@ export default class Particle {
       drawShape.call(this, this.context)
     } else {
       switch(this.shape) {
-        case ParticleShape.Cirlce: {
+        case ParticleShape.Circle: {
           this.context.beginPath()
           this.context.arc(0, 0, this.radius, 0, 2 * Math.PI)
           this.context.fill()
