b33be911d99e2b8738be63581467ca4b7605a741
Aaron Lampros <alampros@gmail.com>
2019-04-03 13:49:32 -0400

    docs: add framerate test story

--- stories/mount-test.stories.jsx
+++ stories/mount-test.stories.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
+import { withKnobs, boolean, number } from '@storybook/addon-knobs'
+import FPSStats from 'react-fps-stats'
 
+import SizedConfetti from './SizedConfetti'
 import ReactConfetti from '../src/ReactConfetti'
 
 const Toggler = ({ children }) => {
@@ -25,7 +28,45 @@ const Toggler = ({ children }) => {
 
 storiesOf('Tests', module)
   .add('Cleanup on mount', () => (
-    <Toggler>
-      <ReactConfetti style={{ zIndex: 0 }} />
-    </Toggler>
+    <div>
+      <Toggler>
+        <ReactConfetti style={{ zIndex: 0 }} />
+      </Toggler>
+    </div>
+  ))
+  .addDecorator(withKnobs)
+  .add('Framerate', () => (
+    <div>
+      <SizedConfetti
+        debug
+        style={{ zIndex: 0 }}
+        run={boolean('Run', true)}
+        recycle={boolean('Recycle', true)}
+        numberOfPieces={number('# Pieces', 200, {
+          range: true,
+          min: 0,
+          max: 2000,
+          step: 10,
+        })}
+        wind={number('Wind', 0, {
+          range: true,
+          min: -0.5,
+          max: 0.5,
+          step: 0.001,
+        })}
+        gravity={number('Gravity', 0.1, {
+          range: true,
+          min: -1,
+          max: 1,
+          step: 0.01,
+        })}
+        opacity={number('Opacity', 100, {
+          range: true,
+          min: 0,
+          max: 100,
+          step: 1,
+        }) / 100}
+      />
+      <FPSStats right={0} left="auto" />
+    </div>
   ))
