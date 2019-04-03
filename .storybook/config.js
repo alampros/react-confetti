937a382580ca03c56f703c64f10b4290b22608a3
Aaron Lampros <alampros@gmail.com>
2019-04-03 09:25:15 -0400

    docs: link to repo from storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,15 @@
-import { configure } from '@storybook/react'
+import { addParameters, configure } from '@storybook/react'
+import { create } from '@storybook/theming'
+addParameters({
+  options: {
+    theme: create({
+      base: 'light',
+      brandTitle: 'React Confetti',
+      brandUrl: 'https://github.com/alampros/react-confetti',
+    }),
+  },
+})
 
-// automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.jsx?$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
