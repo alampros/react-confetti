65e15601e3cd66dee988aed275c1352bd5d076b6
Aaron Lampros <alampros@gmail.com>
2019-04-25 15:53:58 -0400

    docs: add mouse rain demo

--- stories/SizedConfetti.jsx
+++ stories/SizedConfetti.jsx
@@ -3,13 +3,14 @@ import useWindowSize from 'react-use/lib/useWindowSize'
 
 import ReactConfetti from '../src/ReactConfetti'
 
-export default (passedProps) => {
+export default React.forwardRef((passedProps, ref) => {
   const { width, height } = useWindowSize()
   return (
     <ReactConfetti
       width={width}
       height={height}
       {...passedProps}
+      ref={ref}
     />
   )
-}
+})
