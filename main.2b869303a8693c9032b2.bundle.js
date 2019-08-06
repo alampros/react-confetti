636cba24ba619df9e6fa575563da6296fbc69d76
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-06 12:33:07 +0000

    Deploy alampros/react-confetti to github.com/alampros/react-confetti.git:gh-pages

---
+++ main.2b869303a8693c9032b2.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{394:function(n,o,p){p(395),p(497),n.exports=p(879)},497:function(n,o,p){"use strict";p.r(o);p(498),p(737),p(854),p(859)}},[[394,1,2]]]);
