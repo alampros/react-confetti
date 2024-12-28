3be757d656a833966370d0fd5ee0c9fa6a1efe75
Aaron Lampros <alampros@gmail.com>
2024-12-28 08:28:18 -0500

    fix: build multiple module types build cjs, mjs, and browser iife modules. Should fix #162 and #158

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,29 +1,62 @@
-import { babel } from '@rollup/plugin-babel'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
+import pluginTs from '@rollup/plugin-typescript'
 import { RollupOptions } from 'rollup'
+import dts from 'rollup-plugin-dts'
 
-const config: RollupOptions = {
-  input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'module',
-    sourcemap: true,
-    exports: 'default',
-  },
-  external: ['react', 'react-dom', 'tween-functions'],
-  plugins: [
-    commonjs(),
-    babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/env', '@babel/preset-react'],
-      extensions: ['.ts', '.tsx'],
-      exclude: ['src/stories/**/*'],
-    }),
-    typescript({
-      exclude: ['src/stories/**/*', 'rollup.config.ts'],
-    }),
-  ],
+const input = 'src/index.ts'
+const external = ['react', 'react-dom', 'react/jsx-runtime']
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'react/jsx-runtime': 'jsxRuntime',
 }
+
+const config: RollupOptions[] = [
+  {
+    input,
+    output: [
+      {
+        file: 'dist/react-confetti.cjs',
+        format: 'cjs',
+        globals,
+        sourcemap: true,
+      },
+      {
+        file: 'dist/react-confetti.mjs',
+        format: 'es',
+        globals,
+        sourcemap: true,
+      },
+      {
+        name: 'ReactConfetti',
+        file: 'dist/react-confetti.iife.js',
+        format: 'iife',
+        globals,
+        sourcemap: true,
+      },
+      {
+        name: 'ReactConfetti',
+        file: 'dist/react-confetti.umd.js',
+        format: 'umd',
+        globals,
+        sourcemap: true,
+      },
+    ],
+    external,
+    plugins: [
+      pluginTs({
+        exclude: ['src/stories/**/*', 'rollup.config.ts'],
+      }),
+    ],
+  },
+  {
+    input,
+    output: [
+      { format: 'cjs', file: 'dist/react-confetti.d.cts' },
+      { format: 'es', file: 'dist/react-confetti.d.mts' },
+    ],
+    external,
+    plugins: [dts()],
+  },
+]
 
 export default config
